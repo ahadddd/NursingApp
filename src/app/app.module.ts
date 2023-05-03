4829ac03a096aac9ac8f3a53b25b37e71c7563dc
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-03 11:13:03 +0500

    initial routing configured

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './main/app.component';
+import { RouterModule } from '@angular/router';
+import { NursesComponent } from './Nurses/Nurses.component';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        component: NursesComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
