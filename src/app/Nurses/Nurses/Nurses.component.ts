7a90ce36d7a9dbfa171d8ea72d38f3a45adbf8ee
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-03 12:49:32 +0500

    API call successful, data acquired

--- src/app/Nurses/Nurses/Nurses.component.ts
+++ src/app/Nurses/Nurses/Nurses.component.ts
@@ -9,19 +9,28 @@ import { Component, OnInit } from '@angular/core';
 export class NursesComponent implements OnInit {
 
   baseURL = `https://api.digital4nurse.ch/api/public/nurse-search`;
-  resource = `{"sort_by": "name-asc"}`;
-
-  nurses = [];
+  resource = { "sort_by": "name-asc" };
+  loading = '';
+  nurses: any = [];
   constructor(private http: HttpClient) { }
 
   ngOnInit() {
-    
+
   }
 
   getNurses() {
+    this.loading = 'loading';
     const req = this.http.post(this.baseURL, this.resource);
-    console.log(req);
-
+    req.subscribe({
+      next: (res: any) => {
+        this.nurses = res.data;
+        console.log(this.nurses);
+        this.loading = 'done';
+      },
+      error: () => {
+        this.loading = 'error';
+      }
+    })
   }
 
 }
