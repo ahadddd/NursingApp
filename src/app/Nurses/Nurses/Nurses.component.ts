0376fe569adbedc3c74259f5a825764f332c4683
ahadddd <ahad.shehbaz19@gmail.com>
2023-05-03 18:24:20 +0500

    Add/Remove Nurse Functionality Implemented

--- src/app/Nurses/Nurses/Nurses.component.ts
+++ src/app/Nurses/Nurses/Nurses.component.ts
@@ -39,7 +39,28 @@ export class NursesComponent implements OnInit {
   }
 
   deselectAll() {
-    this.selectedNurses = []
+    this.selectedNurses = [];
+  }
+
+  addNurse(index: any) {
+    let exists = false;
+    // console.log(this.nurses[index]);
+    this.selectedNurses.forEach((item: any) => {
+      if(item.id == this.nurses[index].id) {
+        alert('Nurse already added.');
+        exists = true;
+        return;
+      }
+    })
+    if(exists == false) {
+      this.selectedNurses.push(this.nurses[index]);
+      alert('Nurse Added.');
+    }
+  }
+
+  removeNurse(index: any) {
+    console.log(this.selectedNurses[index]);
+    this.selectedNurses.pop(index)
   }
 
 
